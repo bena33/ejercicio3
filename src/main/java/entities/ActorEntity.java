f47a7c2e8496d8953e5a4754850be1f231834086
alvaro <alvbenmor@gmail.com>
2023-12-19 18:30:30 +0100

    subir el proyecto

---
+++ src/main/java/entities/ActorEntity.java
@@ -0,0 +1,69 @@
+package entities;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table(name = "actor", schema = "sakila", catalog = "")
+public class ActorEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "actor_id", nullable = false)
+    private short actorId;
+    @Basic
+    @Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+    @Basic
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+    @Basic
+    @Column(name = "last_update", nullable = false)
+    private Timestamp lastUpdate;
+
+    public short getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(short actorId) {
+        this.actorId = actorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ActorEntity that = (ActorEntity) o;
+        return actorId == that.actorId && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(lastUpdate, that.lastUpdate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(actorId, firstName, lastName, lastUpdate);
+    }
+}
